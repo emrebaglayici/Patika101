936016a301f59d2ff8d001afeee51c27c46326e7
emre <emrebaglayici1@gmail.com>
2022-05-14 17:51:32 +0300

    Pratik

--- src/Giris/BasamakSayisi.java
+++ src/Giris/BasamakSayisi.java
@@ -6,24 +6,14 @@ public class BasamakSayisi {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Sayı giriniz");
-        int digitCounter = 0;
-        int number=scanner.nextInt();
-        int tempNumber=number;
+        int number= scanner.nextInt();
         int result = 0;
         int value;
 
-
-        while (tempNumber!=0){
-            tempNumber/=10;
-            digitCounter++;
-        }
-
-        tempNumber=number;
-
-        while (tempNumber!=0){
-            value=tempNumber%10;
+        while (number!=0){
+            value=number%10;
             result+=value;
-            tempNumber/=10;
+            number/=10;
         }
         System.out.println(result);
 
