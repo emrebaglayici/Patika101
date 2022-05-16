d60251940f30f1b29b1ebfd802fe25d9edfd6b72
emre <emrebaglayici1@gmail.com>
2022-05-16 15:09:42 +0300

    Pratik

---
+++ src/Giris/TersUcgen.java
@@ -0,0 +1,22 @@
+package Giris;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        int n=10;
+        for (int i = n; i >=1 ; i--) {
+            for (int j = 1; j <=(n-i)+1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <=(2*i)-1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        /*
+        for (int j = 1 ; j <=(n-j)+i ; j++) {
+                System.out.print("*");
+            }*
+         */
+
+    }
+}
