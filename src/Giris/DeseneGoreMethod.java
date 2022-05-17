493882ed6d713a761001433ddbc82c9b9414c9a0
emrebaglayici <emrebaglayici1@gmail.com>
2022-05-17 17:08:27 +0300

    Pratik

--- src/Giris/DeseneGoreMethod.java
+++ src/Giris/DeseneGoreMethod.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 
 public class DeseneGoreMethod {
     static void methd(int n){
-        if(n==0 || n<0) {
-            n += 5;
-            System.out.println(n);
-        }
-        else {
+       /* if(n==0 || n<0) {
+            methd(n+5);
+            System.out.print(n+ " ");
+
+        }else{
+            System.out.print(n+ " ");
             methd(n-5);
-            System.out.println(n);
+            System.out.print(n+ " ");
         }
+        */
+        int temp=n;
+        while (n>0){
+            System.out.print(n+ " ");
+            n-=5;
+            if(n<=0){
+                for (int i = n; i <=temp ; i=i+5) {
+                    System.out.print(i+ " ");
+                }
+            }
+        }
+
+
     }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
